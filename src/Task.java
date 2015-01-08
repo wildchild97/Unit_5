fe61a026c4441fadf59c64eb82712786a9675e7b
cait2290 <cait2290@ISS147D8-25.scdsb.pri>
2015-01-08 09:15:19 -0500

    jan 8th

--- src/Task.java
+++ src/Task.java
@@ -1,4 +1,45 @@
 
 public class Task {
     
+    private String name;
+    private String description;
+
+    public Task(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+    
+    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean validate()
+    {
+        if (name ==null || description==null || name.equals("") || description.equals(""))
+        return false;
+        
+        else
+            return true;
+    }
+    
+    public String toString() {
+        return "Task{" + "Name: " + name + "\nDescription: " + description + "\n=======\n";
+    }
+    
+    
+    
 }
